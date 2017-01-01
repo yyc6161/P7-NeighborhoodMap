219ec2ff83acc935f8ea56f9207b883c72a469fb
yyc6161 <yinyuchen61@gmail.com>
2017-01-01 17:36:29 +0800

    Fisrt submit

---
+++ README.md
@@ -0,0 +1,19 @@
+##Neighborhood Map
+This is a website could show you neighborhood map in New York by google map.
+
+###How to run it
+Open the `index.html` in browser or anyone appropriate editor
+
+###Featured
+
+1. Mark New York's interesting spots on Google Maps
+
+	![Locations marker](img/markers.JPG)
+
+2. Show a list of interesting places, you can filter out the places you are not interested by `Filter` button.
+
+	![Locations marker](img/locationList.JPG)
+
+3. Now click the marker on the map you're interested in, it will show you the location information of the marker and the images that **Flickr** users shared.
+
+	![Locations marker](img/markerMessage.JPG)
---
+++ js/script.js
@@ -0,0 +1,342 @@
+"use strict";
+var locations = [
+	{
+		name: "Carnegie Hall",
+		address: "881 7th Ave, New York, NY 10019",
+		location: {lat: 40.765004, lng: -73.97985}
+	},
+	{
+		name: "Central Park Zoo",
+		address: "64th St and 5th Ave, New York, NY 10021",
+		location: {lat: 40.767778, lng: -73.971833}
+	},
+	{
+		name: "Webster Hall",
+		address: "125 E 11th St, New York, NY 10003",
+		location: {lat: 40.731777, lng: -73.989157}
+	},
+	{
+		name: "Mercury Lounge",
+		address: "217 E Houston St, New York, NY 10002",
+		location: {lat: 40.722168, lng: -73.986746}
+	},
+	{
+		name: "The Spotted Pig",
+		address: "314 W 11th St, New York, NY 10014",
+		location: {lat: 40.735607, lng: -74.006671}
+	},
+	{
+		name: "Empire State Building",
+		address: "350 5th Ave, New York, NY 10118",
+		location: {lat: 40.74844, lng: -73.985655}
+	},
+	{
+		name: "Tenement Museum",
+		address: "103 Orchard St, New York, NY 10002",
+		location: {lat: 40.718796, lng: -73.99007}
+	}
+];
+
+/*
+* This function calls a third-party API: the Flickr API, 
+* it parses the json file and stores the address of the associated picture in the array location.
+*/
+function getImgFromFlickr(){
+	var theUrl, imgID, imgFarm, imgServer, imgSecret;
+	locations.forEach(function(location){
+		theUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=56de27d389b39c42f7b663f6ed1f4494&text=' +
+		location.name + '&content_type=1&media=photos&per_page=1&format=json&nojsoncallback=1';
+		$.ajax({ 
+			url: theUrl, 
+			data: JSON.stringify(), 
+			success: function(result, status){
+				if(result.stat === 'ok'){
+        		imgID = result.photos.photo[0].id;
+        		imgFarm = result.photos.photo[0].farm;
+        		imgServer = result.photos.photo[0].server;
+        		imgSecret = result.photos.photo[0].secret;
+        		location.imgUrl = "https://farm"+imgFarm+".staticflickr.com/"+imgServer+"/"+imgID+"_"+imgSecret+"_t.jpg";
+        		}else location.imgUrl = "img/failMessage.png";
+      		},
+      		fail: function(){
+      			location.imgUrl = "img/failMessage.png";
+      	}});
+	});
+	
+}
+getImgFromFlickr();
+
+//This function displays the markers-list in the slide-menu
+function initLocationsList(){
+	var locationsList = $('.locations');
+	locations.forEach(function(location,i){
+		locationsList.append("<li class='location' data-bind='visible: showLocations["+i+"]'><a href='#' class='locLink' id='loc"+
+			i + "' data-bind='click: locationEvent("+i+")'>" + location.name+"</a></li>");
+	});
+}
+initLocationsList();
+
+var map;
+var markers = [];
+var largeInfowindow;
+
+//This function as Google map callback function, its role is to initialize Google Maps
+function initMap() {
+	//Google Maps personalized style
+	var styles = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 33
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2e5d4"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5dac6"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c5c6c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e4d7c6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#fbfaf7"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#acbcc9"
+            }
+        ]
+    }];
+    // Constructor creates a new map.
+    map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 40.7413549, lng: -73.9980244},
+    	zoom: 13,
+    	styles: styles,
+        mapTypeControl: false
+    });
+    largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('26A69A');
+
+    // Create a "highlighted location" marker color for when the user mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('D4E157');
+
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].name;
+        var address = locations[i].address;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i,
+            address: address,
+            icon: defaultIcon,
+        });
+        // Push the marker to array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
+
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);    
+}
+
+/* 
+* This function populates the infowindow when the marker is clicked. We'll only allow
+* one infowindow which will open at the marker that is clicked, and populate based on that markers position.
+*/ 
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div><strong>' + marker.title + '</strong><br>'+marker.address+
+          	'<br><img src="'+locations[marker.id].imgUrl+'">'+'</div>');
+        infowindow.open(map, marker);
+    }
+}
+
+// This function will creat a personalized icon and return
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21,34));
+        return markerImage;
+}
+
+//Hides the specified marker
+function hideMarker(i) {
+    markers[i].setMap(null); 
+}
+//Displays the specified marker
+function showMarker(i) {      
+    markers[i].setMap(map);
+}
+
+/*
+*The function of this function is to filter out the markers do not meet the requirements.
+*/
+function checkFilter(keyword){
+	var tag = false;
+	var bounds = new google.maps.LatLngBounds();
+	locations.forEach(function(loc,i){
+		//Check whether the location name contains keywords
+		if(loc.name.indexOf(keyword)>=0){
+			tag = true;
+			showMarker(i);
+			bounds.extend(markers[i].position);
+			viewModel.showLocations[i](true);
+		}else{
+			hideMarker(i);
+			viewModel.showLocations[i](false);
+		}
+		
+	});
+	// If there is a marker that meets the requirements, fix the boundary 
+	if(tag) map.fitBounds(bounds);
+}
+
+var viewWidth = document.body.offsetWidth;
+
+/*
+* Here is knockout's viewModel
+*/
+var viewModel = {
+	toggleTag: true,
+	menuStatus: ko.observable('menu-hidden'),
+	// This function toggles a class to perform the hiding/showing of slide-menu.
+	changeMenuStatus: function() {
+		if(this.toggleTag) {
+			this.menuStatus('');
+			this.toggleTag = false;
+		}else {
+			this.menuStatus('menu-hidden');
+			this.toggleTag = true;
+		}
+	},
+	filterKeyword: function() {
+		var keyword = $(".keyword").val();
+		checkFilter(keyword);
+	},
+	// If showLocations[i] is true, the corresponding location displays.
+    showLocations: [
+        ko.observable(true),
+        ko.observable(true),
+        ko.observable(true),
+        ko.observable(true),
+        ko.observable(true),
+        ko.observable(true),
+        ko.observable(true)
+    ],
+    // This function handles the event when the mouse encounters the locations-list
+    locationEvent: function(i){
+		$('#loc' + i).on('mouseover',function(){
+			$(this).css("color","#CDDC39");
+			markers[i].setIcon(makeMarkerIcon('D4E157'));
+		});
+
+		$('#loc' + i).on('mouseout',function(){
+		 	$(this).css("color","white");
+		 	markers[i].setIcon(makeMarkerIcon('26A69A'));
+		});
+
+		$('#loc' + i).on('click',function(){
+			if(viewWidth < 800){
+				viewModel.changeMenuStatus();
+			}
+			populateInfoWindow(markers[i], largeInfowindow);
+		});
+    }
+ };
+ko.applyBindings(viewModel);
+
+// If the screen width is greater than 1000px, 
+// I don't think the slide-menu need to be hidden from the beginning
+if(viewWidth > 1000) viewModel.changeMenuStatus();
